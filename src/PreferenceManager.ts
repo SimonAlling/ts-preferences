6fbb8c003696b5d44a67ef3997e2f3d85c8d7663
Simon Alling <alling.simon@gmail.com>
2018-02-19 18:25:24 +0100

    Use ts-storage for localStorage access

--- src/PreferenceManager.ts
+++ src/PreferenceManager.ts
@@ -1,11 +1,13 @@
-import { is, isLike, isNull } from "ts-type-guards";
+import { isLike } from "ts-type-guards";
+import * as Storage from "ts-storage";
+import { assertUnreachable } from "./Utilities";
 import { Preference } from "./preferences/Preference";
 
 export const enum Status {
     OK,
     INVALID_VALUE,
-    PARSE_FAILED,
-    LOCALSTORAGE_FAILURE,
+    JSON_ERROR,
+    LOCALSTORAGE_ERROR,
 }
 
 export type Response<T> = {
@@ -42,53 +44,21 @@ export class PreferenceManager {
 
     public get<T>(preference: Preference<T>): Response<T> {
         const cachedValue: any = this.cache.get(preference);
-        if (isLike(preference.default)(cachedValue)) {
-            // The preference asked for is one of the known ones.
-            try {
-                const savedValue: T | null = this.readFromLocalStorage(preference);
-                if (isNull(savedValue)) {
-                    // There was no saved value.
-                    return {
-                        status: Status.OK,
-                        value: cachedValue,
-                    };
-                } else if (!preference.isValidValue(preference.data, savedValue)) {
-                    // The saved value was not valid.
-                    return {
-                        status: Status.INVALID_VALUE,
-                        value: preference.fromInvalid(savedValue),
-                    };
-                } else {
-                    // There was a valid saved value.
-                    return {
-                        status: Status.OK,
-                        value: savedValue,
-                    };
-                }
-            } catch (err) {
-                let status;
-                if (is(SyntaxError)(err)) {
-                    // The saved value could not be parsed.
-                    status = Status.PARSE_FAILED;
-                } else if (is(TypeError)(err)) {
-                    // The saved value had the wrong type.
-                    status = Status.INVALID_VALUE;
-                } else {
-                    // Something went wrong when trying to access localStorage.
-                    status = Status.LOCALSTORAGE_FAILURE;
-                }
-                return {
-                    status: status,
-                    value: cachedValue,
-                };
-            }
+        if (!isLike(preference.default)(cachedValue)) {
+            // If we got undefined from cache, the preference was not among the known ones.
+            throw new Error(unknown(preference));
         }
-        // If we got undefined from cache, the preference was not among the known ones.
-        throw new Error(unknown(preference));
+        const resp = Storage.get(this.LS_PREFIX + preference.key, preference.default);
+        return {
+            status: fromStorageStatus(resp.status),
+            value: resp.status === Storage.Status.OK ? resp.value : cachedValue,
+        };
     }
 
     public set<T>(preference: Preference<T>, value: T): Response<T> {
-        const getResponse = this.get(preference);
+        if (!isLike(preference.default)(this.cache.get(preference))) {
+            throw new Error(unknown(preference));
+        }
         if (!preference.isValidValue(preference.data, value)) {
             return {
                 status: Status.INVALID_VALUE,
@@ -96,37 +66,21 @@ export class PreferenceManager {
             };
         }
         this.cache.set(preference, value);
-        try {
-            this.saveToLocalStorage(preference, value);
-            return {
-                status: Status.OK,
-                value: value,
-            };
-        } catch {
-            // Something went wrong when trying to access localStorage.
-            return {
-                status: Status.LOCALSTORAGE_FAILURE,
-                value: value,
-            };
-        }
+        const resp = Storage.set(this.LS_PREFIX + preference.key, value);
+        return {
+            status: fromStorageStatus(resp.status),
+            value: resp.value,
+        };
     }
+}
 
-    private readFromLocalStorage<T>(preference: Preference<T>): T | null {
-        // Throws DOMException:
-        const readValue: string | null = localStorage.getItem(this.LS_PREFIX + preference.key);
-        if (isNull(readValue)) {
-            return null;
-        }
-        // Throws SyntaxError:
-        const parsedValue: any = JSON.parse(readValue);
-        if (isLike(preference.default)(parsedValue)) {
-            return parsedValue;
-        }
-        throw new TypeError(`Saved value had wrong type.`);
+function fromStorageStatus(s: Storage.Status): Status {
+    switch (s) {
+        case Storage.Status.OK:                 return Status.OK;
+        case Storage.Status.ABSENT:             return Status.OK;
+        case Storage.Status.TYPE_ERROR:         return Status.INVALID_VALUE;
+        case Storage.Status.JSON_ERROR:         return Status.JSON_ERROR;
+        case Storage.Status.LOCALSTORAGE_ERROR: return Status.LOCALSTORAGE_ERROR;
     }
-
-    private saveToLocalStorage<T>(preference: Preference<T>, value: T): void {
-        // Throws DOMException:
-        localStorage.setItem(this.LS_PREFIX + preference.key, JSON.stringify(value));
-    }
+    return assertUnreachable(s);
 }
--- src/Utilities.ts
+++ src/Utilities.ts
@@ -1,3 +1,7 @@
 export function isInt(x: number): boolean {
     return x % 1 === 0;
 }
+
+export function assertUnreachable(x: never): never {
+    throw new Error("assertUnreachable: " + x);
+}
