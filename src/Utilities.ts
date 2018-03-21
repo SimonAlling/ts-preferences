3176d57efbb97a4f8d9bb466b40e09ac6b4c812c
Simon Alling <alling.simon@gmail.com>
2018-03-21 15:04:42 +0100

    Add multiline and maxLength to StringPreference

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -5,3 +5,7 @@ export function isInt(x: number): boolean {
 export function assertUnreachable(x: never): never {
     throw new Error("assertUnreachable: " + x);
 }
+
+export function fromMaybe<T>(fallback: T, x: T | undefined): T {
+    return x === undefined ? fallback : x;
+}
--- src/preferences/StringPreference.ts
+++ src/preferences/StringPreference.ts
@@ -1,7 +1,48 @@
-import { PreferenceData, Preference } from "./Preference";
+import { PreferenceData, Preference, ParseResult, FromString } from "./Preference";
+import { fromMaybe } from "../Utilities";
 
-export class StringPreference extends Preference<string> {
-    constructor(data: PreferenceData<string>) {
+export type StringPreferenceData = PreferenceData<string> & {
+    multiline: boolean
+    maxLength?: number
+}
+
+export class StringPreference extends Preference<string> implements FromString<string> {
+    static REGEX_LINE_BREAKS = /\n+/g;
+    readonly multiline: boolean;
+    readonly maxLength: number;
+
+    constructor(data: StringPreferenceData) {
         super(data);
+        const maxLength = data.maxLength;
+        if (fromMaybe(Infinity, maxLength) < 0) {
+            throw new Error(`maxLength cannot be negative, but it was ${JSON.stringify(data.maxLength)} for ${this.getType()} '${data.key}'`);
+        }
+        this.multiline = data.multiline;
+        this.maxLength = fromMaybe(Infinity, maxLength);
+    }
+
+    isValidValue(data: StringPreferenceData, value: string): boolean {
+        return (
+            value.length <= fromMaybe(Infinity, data.maxLength)
+            &&
+            (data.multiline || !value.includes("\n"))
+        );
+    }
+
+    fromInvalid(s: string): string {
+        const truncated = s.substring(0, this.maxLength);
+        return this.multiline
+            ? truncated
+            : truncated.replace(StringPreference.REGEX_LINE_BREAKS, " ");
+    }
+
+    fromString(s: string): ParseResult<string> {
+        return (
+            s.length > this.maxLength ? `Max length ${this.maxLength} exceeded.`
+            :
+            !this.multiline && s.includes("\n") ? `Line breaks are not allowed.`
+            :
+            { value: s }
+        );
     }
 }
